9eca44ba17b4d5f90833ed68beb99988c35cda67
Francesco Lazzarotto <francesco.lazzarotto@inaf.it>
2020-10-29 14:12:30 +0100

    Add files via upload

---
+++ coffee.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  puts("..wwwfff... steam ... coffee ready!");
+  printf("please press a key and then enter\n");
+  char string[10];
+  char c = getchar(); 
+  return 0;
+}
